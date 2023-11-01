9a6b517baea4a8e6a07cce0a2b26b20478ac42ac
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2023-11-02 03:10:26 +0900

    Movies Service part One

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -8,18 +8,21 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
 
 @Controller('movies')
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
   @Get()
-  getAll() {
-    return 'This will return all movies';
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
   @Post()
   create(@Body() movieData) {
-    console.log(movieData);
-    return 'This will create a movie';
+    return this.moviesService.create(movieData);
   }
 
   @Get('search')
@@ -28,13 +31,13 @@ export class MoviesController {
   }
 
   @Get(':id')
-  getOne(@Param('id') movieId: string) {
-    return `This will return one movie with the id: ${movieId}`;
+  getOne(@Param('id') movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
   }
 
   @Delete(':id')
-  remove(@Param('id') movieId: string) {
-    return `This will delete a movie with the id: ${movieId}`;
+  remove(@Param('id') movieId: string): boolean {
+    return this.moviesService.deleteOne(movieId);
   }
 
   @Patch(':id')
